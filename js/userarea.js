514c310ba54576c829b842892cb7a47a62f587f5
George Bentes <george.bentes@gmail.com>
2015-12-03 18:50:33 -0400

    Aqui também coloquei a validação de sessão com consulta ao servidor e não mais na parte do browser.

--- js/userarea.js
+++ js/userarea.js
@@ -1,3 +1,10 @@
 $(function(){
-	$(".user-name").text(sessionStorage.getItem("name"));
+	$.get("scripts/verificar_user_logado.php", function(data){
+		if(data.length == 0){
+			location = "forms/login.html";	
+		}else{
+			$(".user-name").text(data.name + "!");
+		}
+	}, 'json');
+
 });
