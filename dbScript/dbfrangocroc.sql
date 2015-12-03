7c732523a7fd0889fbb2d85d537034ba64e62064
George Bentes <george.bentes@gmail.com>
2015-12-03 17:05:46 -0400

    Atualizei o Script do banco de dados

--- dbScript/dbfrangocroc.sql
+++ dbScript/dbfrangocroc.sql
@@ -1,57 +1,57 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+CREATE DATABASE  IF NOT EXISTS `dbfrangocroc` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `dbfrangocroc`;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
 --
--- Host: 127.0.0.1
--- Generation Time: 03-Dez-2015 às 16:36
--- Versão do servidor: 10.1.8-MariaDB
--- PHP Version: 5.6.14
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: dbfrangocroc
+-- ------------------------------------------------------
+-- Server version	5.6.20
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `dbfrangocroc`
+-- Table structure for table `tb_user`
 --
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tb_user`
---
-
+DROP TABLE IF EXISTS `tb_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tb_user` (
   `cpf` varchar(15) NOT NULL,
   `name` varchar(255) NOT NULL,
   `adress` varchar(255) NOT NULL,
   `phone` varchar(25) NOT NULL,
   `mail` varchar(255) NOT NULL,
-  `senha` varchar(255) NOT NULL
+  `senha` varchar(255) NOT NULL,
+  PRIMARY KEY (`cpf`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Extraindo dados da tabela `tb_user`
+-- Dumping data for table `tb_user`
 --
 
-INSERT INTO `tb_user` (`cpf`, `name`, `adress`, `phone`, `mail`, `senha`) VALUES
-('831.641.032-04', 'Matheus Clark dos Santos Monte', 'rua cidadania 96 69097186', '+55 92 995231487', 'casdas@adfas', '123456');
+LOCK TABLES `tb_user` WRITE;
+/*!40000 ALTER TABLE `tb_user` DISABLE KEYS */;
+INSERT INTO `tb_user` VALUES ('123','Satan','admin','666','admin','admin'),('321','user','user','user','user','user'),('700.004.982-03','George Lucas Bentes Nunes','Av Burity 9999 Residencial Eliza Miranda, BL-B, AP 402, AMAP','+55 92 98192-6067','george.bentes@gmail.com','fuck'),('831.641.032-04','Matheus Clark dos Santos Monte','rua cidadania 96 69097186','+55 92 995231487','casdas@adfas','123456');
+/*!40000 ALTER TABLE `tb_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tb_user`
---
-ALTER TABLE `tb_user`
-  ADD PRIMARY KEY (`cpf`);
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-12-03 17:04:59
