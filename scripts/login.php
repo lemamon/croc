5d99cb64df1b6782aa98b237ef3089eff2603302
George Bentes <george.bentes@gmail.com>
2015-12-03 13:58:17 -0400

    Script de login com o banco e criação de sessão

---
+++ scripts/login.php
@@ -0,0 +1,29 @@
+<meta charset="utf-8">
+<?php
+	include_once("conexao.php");
+
+	$user = $_POST["user"];
+	$pass = $_POST["pass"];
+
+	$user = htmlspecialchars($user);
+	$pass = htmlspecialchars($pass);
+
+	$query = "SELECT * FROM tb_user WHERE (phone = '$user' OR mail = '$user') AND senha = '$pass'";
+	$result = mysqli_query($conexao, $query);
+	$num_rows = mysqli_num_rows($result);
+
+	if($num_rows > 0){
+		$dados = mysqli_fetch_array($result);
+
+		session_start();
+		$_SESSION["cpf"] = $dados["cpf"];
+		$_SESSION["user"] = $dados["mail"];
+		$_SESSION["phone"] = $dados["phone"];
+		$_SESSION["name"] = $dados["name"];
+
+		header("location: http://www.google.com");
+	}else{
+		echo "<script>alert('Usuário não encontrado!');location='../forms/login.html'</script>";
+	}
+
+?>
