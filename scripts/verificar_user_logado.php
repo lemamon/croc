0474584fe7a838fd89be2f48dc91d1d76af1cdf1
George Bentes <george.bentes@gmail.com>
2015-12-04 20:13:03 -0400

    Adicionei mais um campo na sessão de verificação que foi justamente o que verifica se o usuário é administrador ou não.

--- scripts/verificar_user_logado.php
+++ scripts/verificar_user_logado.php
@@ -5,7 +5,8 @@
 		$arr = array("cpf"=>$_SESSION["cpf"],
 					 "name"=>$_SESSION["name"],
 					 "mail"=>$_SESSION["mail"],
-					 "phone"=>$_SESSION["phone"]
+					 "phone"=>$_SESSION["phone"],
+					 "admin"=>$_SESSION["admin"]
 					);
 	}
 	echo json_encode($arr, true);	
