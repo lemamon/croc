02575688163264338b305969f6dfb48c02489503
George Bentes <george.bentes@gmail.com>
2015-12-04 20:10:38 -0400

    Coloquei um código que separa a tela de administrador e cliente.

--- js/userarea.js
+++ js/userarea.js
@@ -1,10 +1,27 @@
 $(function(){
 	$.get("scripts/verificar_user_logado.php", function(data){
 		if(data.length == 0){
-			location = "forms/login.html";	
+			location = "forms/login.html";
 		}else{
-			$(".user-name").text(data.name + "!");
+			var username = data.name;
+			username = username.split(" ");
+			if(username.length > 1){
+				username = username[0] + " " + username[username.length-1];
+			}else{
+				username = username[0];
+			}
+
+			
+			$(".user-name").text(username + "!");
+
+			if(data.admin == 1){
+				$(".centro-user").hide();
+				$(".centro-admin").show();
+				$(".userarea-link").text("Área do Administrador");
+			}else{
+				$(".centro-user").show();
+				$(".centro-admin").hide();
+			}
 		}
 	}, 'json');
-
 });
